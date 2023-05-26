44491f9d1af2d632abd7188328e9bf236d628a04
sd0e <sebastiandoe5@gmail.com>
2023-05-26 09:52:26 +0100

    Change gradient location

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,7 @@
 import Layout from '@/components/layout';
 import classes from '@/styles/Auth.module.css';
 import { useRouter } from 'next/router';
+import { Button, TextField, Stack } from '@mui/material';
 
 export default function Auth() {
   const router = useRouter();
@@ -10,6 +11,12 @@ export default function Auth() {
       <div className={classes.content}>
         <div className={classes.inner}>
           <h1>Continue to Chorer</h1>
+          <TextField placeholder="Email" variant="outlined" type="email" />
+          <TextField placeholder="Password" variant="outlined" type="password" />
+          <Stack direction="row" spacing={4}>
+            <Button variant="outlined" color="primary">Sign In</Button>
+            <Button variant="outlined" color="secondary">Sign Up</Button>
+          </Stack>
         </div>
       </div>
     </Layout>
