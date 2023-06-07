f2ba176032f7235d0ca78e104fa514c8fb84cd33
sd0e <sebastiandoe5@gmail.com>
2023-06-07 20:00:46 +0100

    Get authentication on onboarding screen

--- pages/onboarding.tsx
+++ pages/onboarding.tsx
@@ -1,9 +1,11 @@
 import Layout from '@/components/layout';
 import classes from '@/styles/Onboarding.module.css';
 import { useRouter } from 'next/router';
+import { getAuth } from 'firebase/auth';
 
 export default function Onboarding() {
   const router = useRouter();
+  const auth = getAuth();
 
   return (
     <Layout title="Sign Up">
