84bd96a9ba6f63ee94c763032e9e214f206a2ee8
sebastiandoe5 <sebastiandoe5@gmail.com>
2023-07-09 17:39:56 +0100

    Redirect to authentication page if session lost

--- scripts/getSessionFeature.ts
+++ scripts/getSessionFeature.ts
@@ -14,7 +14,11 @@ export enum Feature {
 // returns the value of a session feature in localStorage, preventing user from having to deal with parsing issues
 export default function getSessionFeature(featureName: Feature) {
 	const session = localStorage.session;
-	if (!session) return undefined;
-	const parsedSession = JSON.parse(session);
-	return parsedSession[featureName];
+	if (!session) {
+		window.location.href = '/auth';
+		return undefined;
+	} else {
+		const parsedSession = JSON.parse(session);
+		return parsedSession[featureName];
+	}
 }
