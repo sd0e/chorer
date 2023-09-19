90fdfdb4c72f5af75bd1dc51f8411c0c79816bd5
sd0e <sebastiandoe5@gmail.com>
2023-09-19 10:20:07 +0100

    Update items to use single state

--- components/choreManager.tsx
+++ components/choreManager.tsx
@@ -1,7 +1,7 @@
 // used in both the new chore and chore management pages to create and edit chores
 
 // import necessary libraries
-import { createTheme, Stack, TextField, ThemeProvider, Backdrop, Paper, Select, MenuItem, ListItem } from '@mui/material';
+import { createTheme, Stack, TextField, ThemeProvider, Backdrop, Paper, Select, MenuItem, ListItem, IconButton } from '@mui/material';
 import { useEffect, useState } from 'react';
 import styles from './choreManager.module.css';
 import ActionButton, { ActionButtonColors } from './ui/actionButton';
@@ -12,14 +12,48 @@ import CommonPaper from './ui/commonPaper';
 export type hex = `#${string}`;
 
 // export the component to be used in other files
-export default function ChoreManager({ info }: { info?: { name: string, color: hex, userList: object } | undefined }) {
+export default function ChoreManager({ info }: { info?:
+	{
+		name: string,
+		color: hex,
+		userList: object,
+		repeatTimes: number,
+		repeatFrequency: number,
+		notificationTimeBeforeOverdue: number,
+		completeAction: string,
+		rewardPoints: number,
+		overdueDailyRewardDecrease: number
+	}
+| undefined }) {
 	// define component hooks to temporarily store data
-	const [name, setName] = useState<string>(info ? info.name : '');
-	const [color, setColor] = useState<hex>(info ? info.color : '#000000');
-	const [userList, setUserList] = useState<string>(info ? JSON.stringify(info.userList) : '[]');
+	const [thisInfo, setThisInfo] = useState<string>(() => {
+		let tempInfo = info ? info : {
+			name: '',
+			color: '#000000',
+			userList: [],
+			repeatTimes: 1,
+			repeatFrequency: 604800000,
+			notificationTimeBeforeOverdue: 2,
+			completeAction: '',
+			rewardPoints: 200,
+			overdueDailyRewardDecrease: 50
+		};
+
+		return JSON.stringify(tempInfo);
+	});
+
+	const getProp = (propName: string) => JSON.parse(thisInfo)[propName];
+
+	const updateProp = (propName: string, newValue: any) => {
+		setThisInfo(thisInfo => {
+			let tempInfo = JSON.parse(thisInfo);
+			tempInfo[propName] = newValue;
+			return JSON.stringify(tempInfo);
+		});
+	}
 
 	const newColor = (color: any) => {
-		setColor(color);
+		updateProp('color', color);
 	}
 
 	// initialise a Material UI theme to use the Inter font (used throughout the project)
@@ -59,16 +93,16 @@ export default function ChoreManager({ info }: { info?: { name: string, color: h
 		data.forEach((userInfo: any) => {
 			console.log(userId, userInfo._id);
 			if (userId === userInfo._id) {
-				tempUserList.push(userInfo);
+				let tempUser = userInfo;
+				tempUser["numPerCycle"] = 1;
+				tempUserList.push(tempUser);
 			}
 		});
 
-		setUserList(JSON.stringify(tempUserList));
+		updateProp('userList', tempUserList);
 
 		setUserPopupShowing(false);
 	}
-
-	console.log(JSON.parse(userList));
 
 	return <ThemeProvider theme={theme}>
 		<Backdrop
@@ -87,8 +121,8 @@ export default function ChoreManager({ info }: { info?: { name: string, color: h
 		</Backdrop>
 		<Stack direction="column" spacing={4}>
 			<Stack direction="row" spacing={2}>
-				<TextField value={name} onChange={e => setName(e.target.value)} inputProps={{ style: { fontWeight: 600 } }} label="Chore Name" className={styles.choreName} />
-				<input type="color" className={styles.colorInput} value={color} onChange={e => newColor(e.target.value)} />
+				<TextField value={JSON.parse(thisInfo)["name"]} onChange={e => updateProp('name', e.target.value)} inputProps={{ style: { fontWeight: 600 } }} label="Chore Name" className={styles.choreName} />
+				<input type="color" className={styles.colorInput} value={JSON.parse(thisInfo)["color"]} onChange={e => newColor(e.target.value)} />
 			</Stack>
 			<div>
 				<Stack direction="column" spacing={4} alignItems="flex-start" sx={{ width: '100%' }}>
@@ -96,12 +130,34 @@ export default function ChoreManager({ info }: { info?: { name: string, color: h
 						<h3>Assignees</h3>
 						<ActionButton Icon={AddOutlined} color={ActionButtonColors.Success} onClick={showNewUserPopup}>New</ActionButton>
 					</div>
-					{ userList !== '[]' ? <Stack direction="column" spacing={2} sx={{ width: '100%' }}>{JSON.parse(userList).map((user: any) => {
-						return <CommonPaper>
-							<Stack direction="row" spacing={2} alignItems="center">
-								<span><strong>{user.name}</strong> has this chore for </span>
-								<TextField value="1" sx={{ width: '64px' }} />
-								<span>cycles</span>
+					{ JSON.parse(thisInfo)["userList"].length !== 0 ? <Stack direction="column" spacing={2} sx={{ width: '100%' }}>{JSON.parse(thisInfo)["userList"].map((user: any, idx: number) => {
+						return <CommonPaper key={idx}>
+							<Stack direction="row" spacing={2} alignItems="center" justifyContent="space-between">
+								<Stack direction="row" spacing={2} alignItems="center">
+									<span><strong>{user.name}</strong> has this chore for </span>
+									<TextField value={user.numPerCycle} onChange={e => {
+										let tempUserList = JSON.parse(thisInfo)["userList"];
+										const theNumber = parseInt(e.target.value);
+										if (e.target.value === '') {
+											tempUserList[idx].numPerCycle = 0;
+										} else if (!Number.isNaN(theNumber)) {
+											tempUserList[idx].numPerCycle = Math.abs(theNumber);
+										} else {
+											window.alert('The value must be a number.')
+										}
+										updateProp('userList', tempUserList);
+									}} sx={{ width: '64px' }} />
+									<span>time{user.numPerCycle === 1 ? `` : `s`} per cycle</span>
+								</Stack>
+								<div>
+									<IconButton color="error" onClick={e => {
+										let tempUserList = JSON.parse(thisInfo)["userList"];
+										tempUserList.splice(idx, 1);
+										updateProp('userList', tempUserList);
+									}}>
+										<CancelOutlined />
+									</IconButton>
+								</div>
 							</Stack>
 						</CommonPaper>
 					})}</Stack> : null }
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -2,7 +2,10 @@
 
 import ClientShield from '@/components/clientShield';
 import Layout from '@/components/layout';
+import ActionButton, { ActionButtonColors } from '@/components/ui/actionButton';
+import signOutUser from '@/firebase/signOutUser';
 import styles from '@/styles/CommonPage.module.css';
+import { LogoutOutlined } from '@mui/icons-material';
 import { useRouter } from 'next/router';
 
 // returns the page component to be served over relevant route
@@ -13,7 +16,7 @@ export default function Settings() {
   return (
     <Layout title="Settings" leftMenu>
       <div className={styles.content}>
-        Settings
+        <ActionButton color={ActionButtonColors.Warning} Icon={LogoutOutlined} onClick={() => signOutUser()}>Sign Out</ActionButton>
       </div>
     </Layout>
   )
