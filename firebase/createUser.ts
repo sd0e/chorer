181d1bde19c97b20e84c7bfa29a879c83163ba50
sd0e <sebastiandoe5@gmail.com>
2023-11-07 14:09:50 +0000

    Add error checking

--- firebase/createUser.ts
+++ firebase/createUser.ts
@@ -5,6 +5,14 @@ const auth = getAuth();
 
 const createUser = (email: string, password: string) => {
 	return new Promise((resolve, reject) => {
+		if (!email.includes('@')) {
+			reject('Email is not in the correct format');
+			return;
+		} else if (email.length === 0 || password.length === 0) {
+			reject('The textboxes must have some content');
+			return;
+		}
+
 		createUserWithEmailAndPassword(auth, email, password)
 			.then((result) => {
 				const user = result.user;
--- firebase/signInUser.ts
+++ firebase/signInUser.ts
@@ -6,6 +6,14 @@ const auth = getAuth();
 const signInUser = (email: string, password: string) => {
 	// returns Promise so that client can await the request for whether the request is successful
 	return new Promise((resolve, reject) => {
+		if (!email.includes('@')) {
+			reject('Email is not in the correct format');
+			return;
+		} else if (email.length === 0 || password.length === 0) {
+			reject('The textboxes must have some content');
+			return;
+		}
+		
 		signInWithEmailAndPassword(auth, email, password)
 			.then((result) => {
 				const user = result.user;
