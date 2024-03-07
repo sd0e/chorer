8301f34b848ab8f882b45b093c34c20cf29acfcb
sd0e <sebastiandoe5@gmail.com>
2024-03-07 11:54:42 +0000

    Update details

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,25 +2,13 @@ import { Metadata } from 'next';
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export const metadata: Metadata = {
-  title: "PWA with Next 13",
-  description: "PWA application with Next 13",
+  title: "Chorer",
+  description: "Chorer",
   generator: "Next.js",
   manifest: "/manifest.json",
-  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
   themeColor: "#fff",
-  authors: [
-    { name: "Rajesh Prajapati" },
-    {
-      name: "Rajesh Prajapati",
-      url: "https://www.linkedin.com/in/raazeshp96/",
-    },
-  ],
   viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-  icons: [
-    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
-    { rel: "icon", url: "icons/icon-128x128.png" },
-  ],
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
 };
 
 export default function Document() {
